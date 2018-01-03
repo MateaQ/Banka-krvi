4985486db326dd2061899239350a138b49dc6c1f
Matea Grdić <mgrdic@unipu.hr>
2018-01-03 16:02:58 +0100

    Blonk

--- Banka krvi/Form1.Designer.cs
+++ Banka krvi/Form1.Designer.cs
@@ -62,7 +62,7 @@
 			// pictureBox2
 			// 
 			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-			this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+			this.pictureBox2.Location = new System.Drawing.Point(12, 22);
 			this.pictureBox2.Name = "pictureBox2";
 			this.pictureBox2.Size = new System.Drawing.Size(111, 84);
 			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
--- Banka krvi/Form3.Designer.cs
+++ Banka krvi/Form3.Designer.cs
@@ -57,7 +57,7 @@
 			// 
 			this.button1.BackColor = System.Drawing.Color.LightSalmon;
 			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-			this.button1.Location = new System.Drawing.Point(545, 23);
+			this.button1.Location = new System.Drawing.Point(545, 27);
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(75, 23);
 			this.button1.TabIndex = 4;
